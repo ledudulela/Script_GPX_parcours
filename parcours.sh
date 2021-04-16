4925da370d1ead5da44a8107009d8fb0b3bbbe80
leDuduLéLà <ledudulela@free.fr>
2021-04-16 17:41:41 +0200

    affiche dans la console les fichiers CSV et GPX

--- parcours.sh
+++ parcours.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 python parcours.py -w
+echo  
+echo Appuyez sur une touche pour afficher le CSV...
+read -n 1 ch
+cat parcours.csv
+echo  
+echo Appuyez sur une touche pour afficher le GPX...
+read -n 1 ch
+cat parcours.gpx
+echo  
+echo  
 echo Appuyez sur une touche pour fermer la fenetre...
 read -n 1 ch
+
