f0f6e530570a7b2844b2111a8c896d4944f41937
leDuduLéLà <ledudulela@free.fr>
2021-04-20 22:01:32 +0200

    remplace python par python3 dans la commande

--- parcours.sh
+++ parcours.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
-python parcours.py -w
+echo  
+echo Appuyez sur une touche pour effectuer la recherche...
+read -n 1 ch
+python3 parcours.py -w
 echo  
 echo Appuyez sur une touche pour afficher le CSV...
 read -n 1 ch
 cat parcours.csv
-echo  
-echo Appuyez sur une touche pour afficher le GPX...
-read -n 1 ch
-cat parcours.gpx
 echo  
 echo  
 echo Appuyez sur une touche pour fermer la fenetre...
